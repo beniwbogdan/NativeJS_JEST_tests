ec6f88e90beceea805a094dee43ba026e2d09ab2
beniv_bohdan <bohdanbeniv@gmail.com>
2022-06-26 12:04:31 +0200

    Realize test #2

--- src/02/02.test.ts
+++ src/02/02.test.ts
@@ -39,32 +39,66 @@ beforeEach(() => {
                     }
                 },
             ],
-        governmentBuildings:[],
-        citizensNumber:1000000,
+        governmentBuildings: [{
+            type: "HOSPITAL",
+            budget: 200000,
+            stuffCount: {
+                number: 200
+            },
+            address: {
+                street: {
+                    title: "Central Str"
+                }
+            }
 
-    }})
+        }, {
+            type:"FIRE-STATION",
+            budget:500000,
+            stuffCount:{
+                number:1000
+            },
+            address:{
+                street:{
+                    title:"South Str"
+                }
+            }
+        }],
+        citizensNumber: 1000000,
+
+    }
+})
 
 
-    test("test city should contains 3 houses", () => {
-        expect(city.houses.length).toBe(3);
+test("test city should contains 3 houses", () => {
+    expect(city.houses.length).toBe(3);
 
-        expect(city.houses[0].buildedAt).toBe(2012);
-        expect(city.houses[0].repaired).toBe(false);
-        expect(city.houses[0].address.number).toBe(100);
-        expect(city.houses[0].address.street.title).toBe("White street");
-
-
-        expect(city.houses[1].buildedAt).toBe(2008);
-        expect(city.houses[1].repaired).toBe(false);
-        expect(city.houses[1].address.number).toBe(100);
-        expect(city.houses[1].address.street.title).toBe("Happy street");
+    expect(city.houses[0].buildedAt).toBe(2012);
+    expect(city.houses[0].repaired).toBe(false);
+    expect(city.houses[0].address.number).toBe(100);
+    expect(city.houses[0].address.street.title).toBe("White street");
 
 
-        expect(city.houses[2].buildedAt).toBe(2020);
-        expect(city.houses[2].repaired).toBe(false);
-        expect(city.houses[2].address.number).toBe(101);
-        expect(city.houses[2].address.street.title).toBe("Happy street");
+    expect(city.houses[1].buildedAt).toBe(2008);
+    expect(city.houses[1].repaired).toBe(false);
+    expect(city.houses[1].address.number).toBe(100);
+    expect(city.houses[1].address.street.title).toBe("Happy street");
 
-    })
 
+    expect(city.houses[2].buildedAt).toBe(2020);
+    expect(city.houses[2].repaired).toBe(false);
+    expect(city.houses[2].address.number).toBe(101);
+    expect(city.houses[2].address.street.title).toBe("Happy street");
+
+})
+test("test city should contains hospital and fire station", () => {
+    expect(city.governmentBuildings[0].type).toBe("HOSPITAL");
+    expect(city.governmentBuildings[0].budget).toBe(200000);
+    expect(city.governmentBuildings[0].stuffCount.number).toBe(200);
+    expect(city.governmentBuildings[0].address.street.title).toBe("Central Str");
+
+    expect(city.governmentBuildings[1].type).toBe("FIRE-STATION");
+    expect(city.governmentBuildings[1].budget).toBe(500000);
+    expect(city.governmentBuildings[1].stuffCount.number).toBe(1000);
+    expect(city.governmentBuildings[1].address.street.title).toBe("South Str");
+});
 
--- src/02/02_02.ts
+++ src/02/02_02.ts
@@ -1,19 +1,34 @@
-export type StreetType={
-    title:string
+export type StreetType = {
+    title: string
 }
-export type AddressType={
-    number:number,
-    street:StreetType
+export type AddressType = {
+    number: number,
+    street: StreetType
 
 }
-export type HousesType={
-    buildedAt:number,
-    repaired:boolean,
-    address:AddressType
+export type HousesType = {
+    buildedAt: number,
+    repaired: boolean,
+    address: AddressType
 }
-export type CityType={
-    title:string,
-    houses:Array<HousesType>,
-    governmentBuildings:Array<string>,
-    citizensNumber:number
+export type StuffCount={
+    number:number
+}
+export type GovTitle={
+    title:string
+}
+export type GovSrteetType={
+    street:GovTitle
+}
+export type GovernmentBuildings={
+    type:string,
+    budget:number,
+    stuffCount:StuffCount
+    address:GovSrteetType
+}
+export type CityType = {
+    title: string,
+    houses: Array<HousesType>,
+    governmentBuildings:Array<GovernmentBuildings>,
+    citizensNumber: number
 }
